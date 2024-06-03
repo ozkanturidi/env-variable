f860ac8a198ad58aa9cf7f21322363f2783d88cc
alwaysad <causeimmad@hotmail.com>
2024-06-03 12:46:14 +0300

    yml changes

--- data/database.js
+++ data/database.js
@@ -1,4 +1,4 @@
-import { MongoClient } from 'mongodb';
+import { MongoClient } from "mongodb";
 
 const clusterAddress = process.env.MONGODB_CLUSTER_ADDRESS;
 const dbUser = process.env.MONGODB_USERNAME;
@@ -8,18 +8,22 @@ const dbName = process.env.MONGODB_DB_NAME;
 const uri = `mongodb+srv://${dbUser}:${dbPassword}@${clusterAddress}/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri);
 
-console.log('Trying to connect to db');
+async function connectToDatabase() {
+  console.log("Trying to connect to db");
 
-try {
-  await client.connect();
-  await client.db(dbName).command({ ping: 1 });
-  console.log('Connected successfully to server');
-} catch (error) {
-  console.log('Connection failed.');
-  await client.close();
-  console.log('Connection closed.');
+  try {
+    await client.connect();
+    await client.db(dbName).command({ ping: 1 });
+    console.log("Connected successfully to server");
+    return client.db(dbName);
+  } catch (error) {
+    console.error("Connection failed:", error);
+    await client.close();
+    console.log("Connection closed.");
+    process.exit(1); // Exit the process with failure code
+  }
 }
 
-const database = client.db(dbName);
+const database = await connectToDatabase();
 
 export default database;
